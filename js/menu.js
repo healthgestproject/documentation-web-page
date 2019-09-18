501c05fb3eef40842b41336951318a5796fd374f
healthgestproject <55172623+healthgestproject@users.noreply.github.com>
2019-09-18 14:37:28 -0500

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -14,8 +14,13 @@ var currentScrollPos = window.pageYOffset;
     } else if (prevScrollpos > currentScrollPos && window.pageYOffset < 350){
         document.getElementById("menu").style.top = "0";
         document.getElementById("menu").classList.remove("sticky");
-    } else if (prevScrollpos < currentScrollPos){
-        document.getElementById("menu").style.top = "-65px";
+    } else if (prevScrollpos < currentScrollPos && window.pageYOffset < 350){
+        document.getElementById("menu").style.top = "0px";
+        document.getElementById("menu").classList.remove("sticky");
+        
+    }else if (prevScrollpos < currentScrollPos && window.pageYOffset > 350){
+        document.getElementById("menu").style.top = "0px";
+        document.getElementById("menu").classList.add("sticky");
     }
     prevScrollpos = currentScrollPos;
 }
