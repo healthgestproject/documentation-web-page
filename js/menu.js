56bd6dc1d8adacf929b44bedb726d16a3d018646
healthgestproject <55172623+healthgestproject@users.noreply.github.com>
2019-10-27 20:10:00 -0500

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -11,75 +11,24 @@ var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos && window.pageYOffset > 350) {
 
         document.getElementById("menu").classList.add("sticky");
-        document.getElementById("menu2").classList.add("sticky");
     } else if (prevScrollpos > currentScrollPos && window.pageYOffset < 350){
 
         document.getElementById("menu").classList.remove("sticky");
-        document.getElementById("menu2").classList.remove("sticky");
     } else if (prevScrollpos < currentScrollPos && window.pageYOffset < 350){
 
         document.getElementById("menu").classList.remove("sticky");
-        document.getElementById("menu2").classList.remove("sticky");
         
     }else if (prevScrollpos < currentScrollPos && window.pageYOffset > 350){
 
-        document.getElementById("menu").classList.add("sticky");
-        document.getElementById("menu2").classList.add("sticky");
+        document.getElementById("menu").classList.add("sticky");;
     }
     prevScrollpos = currentScrollPos;
 }
 
-function esconde() {
-  var x = document.getElementById("EAPerú");
-  var y = document.getElementById("EAPerúHide");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    y.src = "img/down.png";
-  } else {
-    x.style.display = "none";
-    y.src = "img/up.png";
-  }
-}
-
-function escondeCol() {
-  var x = document.getElementById("EACol");
-  var y = document.getElementById("EAColHide");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    y.src = "img/down.png";
-  } else {
-    x.style.display = "none";
-    y.src = "img/up.png";
-  }
-}
-
-function escondeEEUU() {
-  var x = document.getElementById("EAEEUU");
-  var y = document.getElementById("EAEEUUHide");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    y.src = "img/down.png";
-  } else {
-    x.style.display = "none";
-    y.src = "img/up.png";
-  }
-}
-function escondeUK() {
-  var x = document.getElementById("EAUK");
-  var y = document.getElementById("EAUKHide");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-    y.src = "img/down.png";
-  } else {
-    x.style.display = "none";
-    y.src = "img/up.png";
-  }
-}
-
 function openWeek() {
-  document.getElementById("mySidebar").style.display = "block";
-}
-
-function closeWeek() {
-  document.getElementById("mySidebar").style.display = "none";
+    if (document.getElementById("mySidebar").style.display == "block"){
+      document.getElementById("mySidebar").style.display = "none";
+    }else{
+        document.getElementById("mySidebar").style.display = "block";
+    }
 }
